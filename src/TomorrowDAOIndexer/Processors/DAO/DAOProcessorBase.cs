457ca47ad9a32e1d53d561c25bbd2c34191e2638
devgantmrwdao <devgan@tmrwdao.com>
2024-07-02 19:01:52 +0800

    feat: remove un use mapper

--- src/TomorrowDAOIndexer/Processors/DAO/DAOProcessorBase.cs
+++ src/TomorrowDAOIndexer/Processors/DAO/DAOProcessorBase.cs
@@ -1,12 +1,9 @@
 using AElf.CSharp.Core;
-using Volo.Abp.ObjectMapping;
 
 namespace TomorrowDAOIndexer.Processors.DAO;
 
 public abstract class DAOProcessorBase<TEvent> : ProcessorBase<TEvent> where TEvent : IEvent<TEvent>, new()
 {
-    protected IObjectMapper ObjectMapper => LazyServiceProvider.LazyGetRequiredService<IObjectMapper>();
-    
     public override string GetContractAddress(string chainId)
     {
         return chainId switch
