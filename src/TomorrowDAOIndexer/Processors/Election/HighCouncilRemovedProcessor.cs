0f871a87be8d259add746b024f020ca50e31760e
devgantmrwdao <devgan@tmrwdao.com>
2024-07-08 13:52:15 +0800

    feat: fix code audit, use foreach temporarily

--- src/TomorrowDAOIndexer/Processors/Election/HighCouncilRemovedProcessor.cs
+++ src/TomorrowDAOIndexer/Processors/Election/HighCouncilRemovedProcessor.cs
@@ -3,7 +3,6 @@ using AeFinder.Sdk.Processor;
 using TomorrowDAO.Contracts.Election;
 using TomorrowDAO.Indexer.Plugin;
 using TomorrowDAOIndexer.Entities;
-using Volo.Abp.DependencyInjection;
 
 namespace TomorrowDAOIndexer.Processors.Election;
 
@@ -16,10 +15,13 @@ public class HighCouncilRemovedProcessor : ElectionProcessorBase<HighCouncilRemo
         Logger.LogInformation("[HighCouncilRemoved] START: DaoId={Id}, ChainId={ChainId}", daoId, chainId);
         try
         {
-            var addressList = logEvent.RemoveHighCouncils.Value.Select(address => address.ToBase58()).ToList();
-
+            var addressList = new List<string>();
+            foreach (var address in logEvent.RemoveHighCouncils.Value)
+            {
+                addressList.Add(address.ToBase58());
+            }
+            // var addressList = logEvent.RemoveHighCouncils.Value.Select(address => address.ToBase58()).ToList();
             var highCouncilConfigId = IdGenerateHelper.GetId(daoId, chainId);
-
             var highCouncilConfigIndex = await GetEntityAsync<ElectionHighCouncilConfigIndex>(highCouncilConfigId);
             if (highCouncilConfigIndex == null)
             {
@@ -28,12 +30,20 @@ public class HighCouncilRemovedProcessor : ElectionProcessorBase<HighCouncilRemo
                     daoId, chainId);
                 return;
             }
-
+            
             Logger.LogInformation(
                 "[HighCouncilRemoved] ElectionHighCouncilConfigIndex existed: DaoId={Id}, ChainId={ChainId}",
                 daoId, chainId);
-            highCouncilConfigIndex.InitialHighCouncilMembers.RemoveAll(item => addressList.Contains(item));
-
+            var councilMembers = new List<string>();
+            foreach (var member in highCouncilConfigIndex.InitialHighCouncilMembers)
+            {
+                if (!addressList.Contains(member))
+                {
+                    councilMembers.Add(member);
+                }
+            }
+            highCouncilConfigIndex.InitialHighCouncilMembers = councilMembers;
+            // highCouncilConfigIndex.InitialHighCouncilMembers.RemoveAll(item => addressList.Contains(item));
             await SaveEntityAsync(highCouncilConfigIndex, context);
             Logger.LogInformation(
                 "[HighCouncilRemoved] Update ElectionHighCouncilConfigIndex FINISH: Id={Id}, ChainId={ChainId}",
