3540a094c6484d24728aedc9d0a272208c54dcce
strongman6693 <strongman@tmrwdao.com>
2024-07-10 17:32:27 +0800

    feat: Governance graphql

--- src/TomorrowDAOIndexer/GraphQL/Input/GovernanceSchemeIndexInput.cs
+++ src/TomorrowDAOIndexer/GraphQL/Input/GovernanceSchemeIndexInput.cs
@@ -2,6 +2,6 @@ namespace TomorrowDAOIndexer.GraphQL.Input;
 
 public class GovernanceSchemeIndexInput
 {
-    public string ChainId { get; set; }
-    public string DAOId { get; set; }
+    public string? ChainId { get; set; }
+    public string? DAOId { get; set; }
 }
