f76a3632876ac79876c475848288e2b5330b82d7
devgantmrwdao <devgan@tmrwdao.com>
2024-09-16 17:50:17 +0800

    feat: block height

---
+++ test/TomorrowDAOIndexer.Tests/GraphQL/TokenQueryTest.cs
@@ -0,0 +1,20 @@
+using TomorrowDAOIndexer.GraphQL.Input;
+using Xunit;
+
+namespace TomorrowDAOIndexer.GraphQL;
+
+public partial class QueryTest
+{
+    [Fact]
+    public async Task GetSyncUserBalanceInfosAsync_Test()
+    {
+        // await IssuedProcessor.ProcessAsync(GenerateLogEventContext(tDVW, Issued()));
+        //
+        // var result = await Query.GetSyncUserBalanceInfosAsync(UserBalanceIndexRepository, ObjectMapper, new GetChainBlockHeightInput
+        // {
+        //     StartBlockHeight = BlockHeight, EndBlockHeight = BlockHeight + 1,
+        //     ChainId = tDVW, SkipCount = 0, MaxResultCount = 10
+        // });
+        // var a = 0;
+    }
+}
